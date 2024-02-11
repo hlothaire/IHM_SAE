edeefdd5fc315c36b642361b8e29b1c627b229d7
hlothaire <lothaire.holcvart78@gmail.com>
2024-02-11 17:36:03 +0100

    modify init

--- src/controleur/intervention_liste.ts
+++ src/controleur/intervention_liste.ts
@@ -1,3 +1,7 @@
 import { vueInterventionListe } from "./class_intervention_liste";
 
-vueInterventionListe.init();
+vueInterventionListe.init({
+  divTitre: document.querySelector("[id=divtitre]"),
+  tableIntervention: document.querySelector("[id=table_intervention]"),
+  btnAjouter: document.querySelector("[id=btn_intervention_ajouter]"),
+});
